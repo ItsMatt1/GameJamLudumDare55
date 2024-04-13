deb11ccdf83e01bf8339e8acba80bd055f04eeea
Matheus de Moura Rosa <matheusmoura@arcadium-studios.com>
2024-04-13 14:53:49 -0300

    Minor updates

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene1"); // Load the game scene
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("OptionsScene"); // Load the options scene
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saiu");
+        Application.Quit(); // Quit the application
+    }
+    
+    
+}
