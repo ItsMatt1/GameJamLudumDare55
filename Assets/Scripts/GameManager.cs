c075e0b944747962f8ba4c5046a47fbb212946c0
Cley Junior <jr.blcey@gmail.com>
2024-04-14 18:04:22 -0300

    feat: Adding sprites

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
     public DemonButton ChoosedDemon { get; set; }
+
+    private int mana;
+
+    [SerializeField]
+    private Text manaTxt;
+
+    public int Mana
+    {
+        get
+        {
+            return mana;
+        }
+        set
+        {
+            this.mana = value;
+            this.manaTxt.text = "<color=#361515> Mana: " + value.ToString() + "</color>";
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mana = 80;
     }
 
     // Update is called once per frame
