30647fae69a2818c98c132b72bec8411d9af622b
Lucas Fernandes <lucasfernandesprogrammer@gmail.com>
2024-04-14 17:40:21 -0300

    feat: aplicação de efeitos sonoros menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,11 +4,20 @@ using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class AuxAudioManager
+{
+    public static bool BackButtonPressed = false;
+    
+}
+
 public class MenuManager : MonoBehaviour
 {
     public void Start()
     {
-        AudioManager.instance.Play("MenuSong");
+        if (SceneManager.GetActiveScene().name.Equals("MenuScene") && AuxAudioManager.BackButtonPressed == false)
+        {
+            AudioManager.instance.Play("MenuSong");
+        }
     }
 
     public void PlayGame()
@@ -20,17 +29,26 @@ public class MenuManager : MonoBehaviour
 
     public void OpenOptions()
     {
+        SelectedMenu();
         SceneManager.LoadScene("OptionsScene"); // Load the options scene
+    }
+
+    public void SelectedMenu()
+    {
+        AudioManager.instance.Play("SelectMenu");
     }
 
     public void QuitGame()
     {
+        SelectedMenu();
         Debug.Log("Saiu");
         Application.Quit(); // Quit the application
     }
 
     public void BackButton()
     {
+        SelectedMenu();
+        AuxAudioManager.BackButtonPressed = true;
         SceneManager.LoadScene("MenuScene");
     }
     
